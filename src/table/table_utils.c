4c7c6c38a3172f5d737fed35d8466c999c1e2203
Dan <danilodevs@gmail.com>
2026-06-13 10:15:44 +0200

    test: add bash test runner for philosopher scenarios

--- src/table/table_utils.c
+++ src/table/table_utils.c
@@ -7,14 +7,6 @@ void	table_free(t_table *table)
 	if (!table)
 		return ;
 
-	pthread_mutex_destroy(&table->printf_mutex);
-	
-	for (int i = 0; i < table->config.philo_count; i++)
-	{
-		pthread_mutex_destroy(&table->forks[i]);
-		pthread_mutex_destroy(&table->philosophers[i].mutex);
-	}
-
 	free(table->forks);
 	free(table->philosophers);
 }
